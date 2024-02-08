d3ff3bea8ef58792015594edda09e91e1886dfb9
joao.felipe <joao.felipe@propig.com.br>
2024-02-08 18:26:02 -0300

    [ fix ] -> changing the function ExecutaQuery to a helper for usability

--- src/v1/controllers/controller_current_climate.go
+++ src/v1/controllers/controller_current_climate.go
@@ -3,8 +3,6 @@ package controllers
 import (
 	"api/src/v1/helpers"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -26,23 +24,8 @@ func GetCurrentClimate(w http.ResponseWriter, r *http.Request) {
 
 	query := helpers.BuildQueryCurrentCondition(fmt.Sprintf("%f", geolocation["lat"]), fmt.Sprintf("%f", geolocation["lng"]))
 
-	result := executeQuery(query)
+	result := helpers.ExecuteQuery(query)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(result))
-}
-
-func executeQuery(query string) string {
-	resp, err := http.Get(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(body)
 }
